3a974be31682ca297a71828962bb112eb347b525
bwilliford <bwilliford@gmail.com>
2021-04-28 12:53:40 -0500

    Various

--- js/chalkboard.js
+++ js/chalkboard.js
@@ -1,6 +1,8 @@
 //Initialize DOM elements we'll manipulate
 let chalkboard = document.getElementById('chalkboard');
 let toggleBG = document.getElementById('toggleBackground');
+let toolbar = document.getElementById('toolbar');
+let mainColor = document.getElementById('mainColor');
 
 //Initialization of PaperJS and attaching to canvas
 paper.install(window);
@@ -29,19 +31,49 @@ chalk.onMouseDrag = function(event) {
     path.add(event.point);
 }
 chalk.onMouseUp = function(event) {
-    sketch.push(path);
+    if (strokeColor === 'white' || strokeColor === 'black') {
+        sketch.push(path);
+    }
 }
+
+//Eraser tool
+let eraser = new paper.Tool();
+eraser.onMouseDown = erase;
+eraser.onMouseDrag = erase;
+function erase(event) {
+    //dataChanged = true;
+    let hits = paper.project.hitTestAll(event.point, {
+        segments: true,
+        fill: true,
+        class: paper.Path,
+        tolerance: 5,
+        stroke: true
+    });
+
+    if (hits.length) {
+        for (var i = 0; i < hits.length; i++) {
+            let hit = hits[i];
+            hit.item.remove();            
+        }
+    }
+}
+
+
+
+//Main functions
 
 //Toggle background from chalkboard to whiteboard and vice-versa
 function toggleBackground() {
     if (chalkboard.className === "chalkboard") {
         chalkboard.className = "whiteboard";
-        toggleBG.textContent = "Whiteboard";
+        toggleBG.textContent = "Switch to Chalkboard";
+        mainColor.style.backgroundColor = "#000000";
         toggleStrokeColors('black');
     }
     else {
         chalkboard.className = "chalkboard";
-        toggleBG.textContent = "Chalkboard";
+        toggleBG.textContent = "Switch to Whiteboard";
+        mainColor.style.backgroundColor = "#ffffff";
         toggleStrokeColors('white');
     }
 }
@@ -53,4 +85,50 @@ function toggleStrokeColors(color) {
             sketch[i].strokeColor = color;
             strokeColor = color;
     }
+}
+
+//Change colors
+function changeColor(color) {
+    //Reset active classes
+    for (let i = 0; i < toolbar.children.length; i++) {
+        toolbar.children[i].className = "";
+    }
+
+    //Activate drawing tool
+    chalk.activate();
+
+    //Change color
+    document.getElementById(color).className = "active";
+    if (color === 'mainColor') {
+        if (chalkboard.className === "chalkboard") {
+            strokeColor = 'white';
+        }
+        else {
+            strokeColor = 'black';
+        }
+    }
+    else if (color === 'red') {
+        strokeColor = '#df5959'; 
+    }
+    else if (color === 'blue') {
+        strokeColor = '#678fe0'; 
+    }
+    else if (color === 'green') {
+        strokeColor = '#86d0ca'; 
+    }
+    else if (color === 'purple') {
+        strokeColor = '#9e7ad9'; 
+    }
+    else if (color === 'orange') {
+        strokeColor = '#d69e54'; 
+    }
+}
+
+function toggleEraser() {
+    //Reset active classes
+    for (let i = 0; i < toolbar.children.length; i++) {
+        toolbar.children[i].className = "";
+    }
+    document.getElementById('eraser').className = "active";
+    eraser.activate();
 }
