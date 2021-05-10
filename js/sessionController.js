d61928f6e1776522804b7d9dacb512db6f5b1dcb
bwilliford <bwilliford@gmail.com>
2021-05-10 13:49:13 -0500

    various things

---
+++ js/sessionController.js
@@ -0,0 +1,126 @@
+window.onload = (event) => {
+    let tileList = document.getElementById('tileList');
+
+    for (i = 1; i < 13; i++) {
+        let num = i.toString();
+
+        let tile = document.createElement('li');
+        tile.id = "tile"+num;
+        tile.className = "tile";
+
+        let tileImage = document.createElement('div');
+        tileImage.className = "tileImage";
+        tileImage.style.backgroundImage = "url('./images/people/"+i+".jpg')";
+        
+        tileImage.onclick = function() {
+            expand("tile"+num);
+        }
+        tile.appendChild(tileImage);
+        
+        let name = document.createElement('div');
+        name.className = "name";
+        name.textContent = "Student "+num;
+        
+        let videoToggle = document.createElement('div');
+        videoToggle.className = "videoToggle"
+        videoToggle.id = "video"+num;
+        videoToggle.onclick = function() {
+            toggleVideo('video'+num)
+        }
+        let audioToggle = document.createElement('div');
+        audioToggle.className = "audioToggle"
+        audioToggle.id = "audio"+num.toString();;
+        audioToggle.onclick = function() {
+            toggleAudio('audio'+num)
+        }
+        name.appendChild(videoToggle);
+        name.appendChild(audioToggle);
+        tile.appendChild(name);
+        tileList.appendChild(tile);
+    }
+};
+
+function toggleVideo(tile) {
+    if (document.getElementById(tile).className === 'videoToggle') {
+        document.getElementById(tile).className = 'videoToggle off';
+    }
+    else {
+        document.getElementById(tile).className = 'videoToggle';
+    }
+}
+
+function toggleAudio(tile) {
+    if (document.getElementById(tile).className === 'audioToggle') {
+        document.getElementById(tile).className = 'audioToggle off';
+    }
+    else {
+        document.getElementById(tile).className = 'audioToggle';
+    }
+}
+
+function expand(tile) {
+    if (document.getElementById(tile).className === 'tile') {
+        //Reset any other tiles
+        let ul = document.getElementById("tileList");
+        let items = ul.getElementsByTagName("li");
+        for (let i = 0; i < items.length; i++) {
+            items[i].className = 'tile';
+        }
+        document.getElementById(tile).className = 'tile expanded';
+    }
+    else if (document.getElementById(tile).className === 'tile expanded') {
+        document.getElementById(tile).className = 'tile';
+    }
+}
+
+function toggleInformation(tool) {
+    if (document.getElementById(tool).className === 'active') {
+        document.getElementById(tool).className = '';
+    }
+    else {
+        document.getElementById(tool).className = 'active';
+    }
+}
+
+function togglePanel(panel) {
+    if (document.getElementById('panel').className === 'active') {
+        document.getElementById('panel').className = '';
+        document.getElementById('panel-'+panel).style.display = 'none';
+        document.getElementById(panel).className = '';
+        //Chat specific
+        if (panel === 'chat') {
+        document.getElementById('chatThread').className = '';
+        }
+    }
+    else {
+        document.getElementById('panel').className = 'active';
+        document.getElementById('panel-'+panel).style.display = 'block';
+        document.getElementById(panel).className = 'active';
+    }
+}
+
+function toggleChalkboard() {
+    if (document.getElementById('chalkboardContainer').className === 'active') {
+    document.getElementById('chalkboardContainer').className = '';
+    document.getElementById('chalk').className = '';
+    }
+    else {
+    document.getElementById('chalkboardContainer').className = 'active';
+    document.getElementById('chalk').className = 'active';
+    if (!chalkActivated) {
+        setTimeout( function() {
+        initializePaper();
+        }, 1000);      
+    }
+    }
+}
+
+function toggleThread(thread) {
+    if (thread === 'exit') {
+        document.getElementById('chatThread').className = '';
+    }
+    else {
+        document.getElementById('chatThread').className = 'active';
+        document.getElementById('threadName').textContent = thread;
+    }
+}    
