f5a088b02ff87ef59ba6fafaae1a11c5ee1ab682
Blake Williford <bwilliford@gmail.com>
2024-03-22 16:18:40 -0400

    Life update

--- js/life-old.js
+++ js/life-old.js
@@ -1,10 +1,9 @@
 let canvas = document.getElementById('lifeCanvas');
 let canvasHeight = 0;
 let canvasWidth = 0;
-let tileSize = 16;
+let tileSize = 40;
 
 let grid = [];
-let newGrid = [];
 
 let gameActive;
 let gameSpeed;
@@ -59,13 +58,8 @@ function startGame() {
     for (let i = 0; i < (canvasWidth/tileSize); i++) {
             grid[i] = [];
             for (let j = 0; j < (canvasHeight/tileSize); j++) {
-                let random = Math.floor(Math.random()*2);
-                if (random == 1) {
-                    grid[i][j] = "alive";
-                }
-                else {
-                    grid[i][j] = "dead";
-                }
+                let random = Math.floor(Math.random()*3);
+                grid[i][j] = random;
             }
         }
 
@@ -94,71 +88,45 @@ function startGame() {
 
 function evolve() {
     //update values
-    newGrid = [];
-
+    let newGrid = [];
     for (let i = 0; i < grid.length; i++) {
         newGrid[i] = [];
         for (let j = 0; j < grid[i].length; j++) {
             let neighbors = checkNeighbors(i,j);
             
-            //Fade
-            if (grid[i][j] == "dying8") {
-                newGrid[i][j] = "dying8";
-            }
-            else if (grid[i][j] == "dying7") {
-                newGrid[i][j] = "dying7";
-            }
-            else if (grid[i][j] == "dying6") {
-                newGrid[i][j] = "dying6";
-            }
-            else if (grid[i][j] == "dying5") {
-                newGrid[i][j] = "dying5";
-            }
-            else if (grid[i][j] == "dying4") {
-                newGrid[i][j] = "dying4";
-            }
-            else if (grid[i][j] == "dying3") {
-                newGrid[i][j] = "dying3";
-            }
-            else if (grid[i][j] == "dying2") {
-                newGrid[i][j] = "dying2";
-            }
-            else if (grid[i][j] == "dying1") {
-                newGrid[i][j] = "dying1";
-            }
-            else if (grid[i][j] == "dying0") {
-                newGrid[i][j] = "dying0";
-            }
-
             //Is cell  alive?
-            if (grid[i][j] == "alive") {
+            if (grid[i][j] > 0) {
                 //Cell dies
                 if (neighbors < 2) {
-                    newGrid[i][j] = "dead";
+                    newGrid[i][j] = 0;
                 }
 
                 //Cell lives
                 if (neighbors === 2) {
-                    newGrid[i][j] = "alive";
+                    newGrid[i][j] = 2;
                 }
 
                 //Cell lives
                 if (neighbors === 3) {
-                    newGrid[i][j] = "alive";
+                    newGrid[i][j] = 2;
                 }
 
                 //Cell overpopulated
                 if (neighbors > 3) {
-                    newGrid[i][j] = "dead";
+                    newGrid[i][j] = 0;
                 }
             }
             //No
             else {
                 //Cell ressurected
                 if (neighbors === 3) {
-                    newGrid[i][j] = "alive";
+                    newGrid[i][j] = 2;
+                }
+                else {
+                    newGrid[i][j] = 0;
                 }
             }
+
         }
     }
 
@@ -183,59 +151,30 @@ function renderCells() {
             let y = (j*tileSize) + (tileSize/2);
             let point = new Point(x,y);
             let cell;
-            if (grid[i][j] === "dead") {
-                cell = new Path.Circle(point, tileSize/2.2);
-                cell.fillColor = 'rgba(255,255,255,0.9)';  
-                grid[i][j] = "dying8";
+            if (grid[i][j] === 0) {
+                cell = new Path.Circle(point, tileSize/2);
+                cell.fillColor = 'rgba(255,255,255,1)';  
+                deathFade(cell, 1);
             }
-            else if (grid[i][j] === "dying8") {
-                cell = new Path.Circle(point, tileSize/2.4);
-                cell.fillColor = 'rgba(255,255,255,0.8)'; 
-                grid[i][j] = "dying7";
-            }
-            else if (grid[i][j] === "dying7") {
-                cell = new Path.Circle(point, tileSize/2.6);
-                cell.fillColor = 'rgba(255,255,255,0.7)'; 
-                grid[i][j] = "dying6";
-            }
-            else if (grid[i][j] === "dying6") {
-                cell = new Path.Circle(point, tileSize/2.8);
-                cell.fillColor = 'rgba(255,255,255,0.6)'; 
-                grid[i][j] = "dying5";
-            }
-            else if (grid[i][j] === "dying5") {
-                cell = new Path.Circle(point, tileSize/3);
+            else if (grid[i][j] === 1)  {
+                cell = new Path.Circle(point, tileSize/2);    
                 cell.fillColor = 'rgba(255,255,255,0.5)'; 
-                grid[i][j] = "dying4";
             }
-            else if (grid[i][j] === "dying4") {
-                cell = new Path.Circle(point, tileSize/3.2);
-                cell.fillColor = 'rgba(255,255,255,0.4)';  
-                grid[i][j] = "dying3";
-            }
-            else if (grid[i][j] === "dying3") {
-                cell = new Path.Circle(point, tileSize/3.4);
-                cell.fillColor = 'rgba(255,255,255,0.3)';  
-                grid[i][j] = "dying2";
-            }
-            else if (grid[i][j] === "dying2") {
-                cell = new Path.Circle(point, tileSize/3.6);
-                cell.fillColor = 'rgba(255,255,255,0.2)';  
-                grid[i][j] = "dying1";
-            }
-            else if (grid[i][j] === "dying1") {
-                cell = new Path.Circle(point, tileSize/3.8);
-                cell.fillColor = 'rgba(255,255,255,0.1)';  
-                grid[i][j] = "dying0";
-            }
-            else if (grid[i][j] === "dying0") {
-                
-            }
-            else if (grid[i][j] === "alive")  {  
+            else if (grid[i][j] === 2)  {  
                 cell = new Path.Circle(point, tileSize/2);    
                 cell.fillColor = 'rgba(255,255,255,1)'; 
             } 
         }
+    }
+}
+
+function deathFade(cell, opacity) {
+    opacity = opacity - 0.1;
+    cell.opacity = opacity;
+    if (opacity > 0) {
+        setTimeout(() => {
+            deathFade(cell, opacity); 
+        }, 100);
     }
 }
 
@@ -244,49 +183,49 @@ function checkNeighbors(x,y) {
     
     if (x > 0) {
         //west
-        if (grid[x-1][y] == "alive") {
+        if (grid[x-1][y] > 0) {
             neighbors++;
         }
     }
     if (x < grid.length-1) {
         //east
-        if (grid[x+1][y] == "alive") {
+        if (grid[x+1][y] > 0) {
             neighbors++;
         }
     }
     if (y > 0) {
         //north
-        if (grid[x][y-1] == "alive") {
+        if (grid[x][y-1] > 0) {
             neighbors++;
         }
     }
     if (y < grid[x].length-1) {
         //south
-        if (grid[x][y+1] == "alive") {
+        if (grid[x][y+1] > 0) {
             neighbors++;
         }
     }
     if (x > 0 && y > 0) {
         //northwest
-        if (grid[x-1][y-1] == "alive") {
+        if (grid[x-1][y-1] > 0) {
             neighbors++;
         }
     }
     if (x < grid.length-1 && y < grid[x].length-1) {
         //southeast
-        if (grid[x+1][y+1] == "alive") {
+        if (grid[x+1][y+1] > 0) {
             neighbors++;
         }
     }
     if (x > 0 && y < grid[x].length-1) {
         //southwest
-        if (grid[x-1][y+1] == "alive") {
+        if (grid[x-1][y+1] > 0) {
             neighbors++;
         }  
     }
     if (x < grid.length-1 && y > 0) {
         //southwest
-        if (grid[x+1][y-1] == "alive") {
+        if (grid[x+1][y-1] > 0) {
             neighbors++;
         }  
     }    
